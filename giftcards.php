62b658e729e6036c064db06d3f0b5e7fd1c039c8
Julian Poyourow <julianpoyo@gmail.com>
2015-07-13 16:46:08 -0700

    Added a giftcards script

---
+++ giftcards.php
@@ -0,0 +1,65 @@
+<?php
+
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
+
+
+include 'Slim/Slim.php';
+
+require 'ProtectedDocs/connection.php';
+
+$app = new Slim();
+
+$app->post('/giftcards', 'createCard');
+$app->put('/giftcards', 'updateCard');
+$app->get('/giftcards/:id', 'getCard');
+$app->get('/giftcards', 'getCards');
+//$app->delete('/giftcards', 'deleteCard');
+
+$app->run();
+
+function createCard(){
+
+}
+
+function updateCard(){
+
+}
+
+function getCard(){
+
+}
+
+function getCards(){
+    
+}
+
+function utf8ize($mixed) {
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } else if (is_string ($mixed)) {
+        return utf8_encode($mixed);
+    }
+    return $mixed;
+}
+
+?>
